5a0b85190469c6ac3086b3a0e69b40ae6a7ebe0f
avichai <avichai.bendavid@mail.huji.ac.il>
2017-06-27 13:56:20 +0300

    commit

--- coin_tosses.py
+++ coin_tosses.py
@@ -1,48 +1,53 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# init data
 data = np.random.binomial(1, 0.25, (100000, 1000))
 
 epsilon = [0.5, 0.25, 0.1, 0.01, 0.001]
-# epsilon1 = [0.5]
-
 colors = ['b', 'y', 'g', 'k', 'r']
-
 n_rows = data.shape[0]
 n_cols = data.shape[1]
+xsis = np.arange(1, n_cols + 1)
 
-# todo check if the following lines needs to be done for each eps since it not related to epse and will result the same all the time
+# 5.2.a
 plt.figure()
 for i in range(5):
-    avg = np.sum(data[i, :])/n_cols
-    plt.plot([avg, avg], colors[i])
+    cumsum = np.cumsum(data[i, :])
+    norm_cumsum = 1.0 * cumsum / xsis
+    label_seq = 'seq ' + str(i)
+    plt.plot(xsis, norm_cumsum, colors[i], label=label_seq)
+
+    plt.title('p estimator')
+    plt.xlabel('m')
+    plt.ylabel('p estimator')
+plt.legend(loc='upper right')
 plt.show()
 
-delta = np.linspace(0.001, 0.999, 100)
-
+# 5.2.(b+c)
 for eps in epsilon:
-    eps_square = np.square(eps)
-    plt.figure()
+    chebyshev = 1.0 / (4 * xsis * (eps ** 2))
+    hofding = 2 * np.exp(-2 * xsis * (eps ** 2))
+
+    plt.plot(xsis, chebyshev, 'b', label='Chebyshev')
+    plt.plot(xsis, hofding, 'g', label='Hoffding')
+
     plt.title('epsilon = {0}'.format(eps))
-    plt.xlabel('delta')
-    plt.ylabel('m')
-    m1 = (1/(4*eps_square))*(1/delta)
-    m2 = (1/(2*eps_square))*(np.log(2/delta))
-    plt.plot(delta, m1, 'b')
-    plt.plot(delta, m2, 'g')
+    plt.xlabel('m')
+    plt.ylabel('upper bound')
 
-    # count = 0
-    # f_eps = 0.25
-    # for i in range(n_rows):
-    #     avg = np.sum(data[i, :]) / n_cols
-    #     if np.abs(avg - f_eps) >= eps:
-    #         count += 1
-    # perc = count/n_rows
-    # print(perc)
-    # plt.plot(perc, 'r')
+    cumsum = np.cumsum(data, 1)
+    norm_cumsum = 1.0 * cumsum / xsis
+    norm_cumsum_minus_expectation = norm_cumsum - 0.25
+    greater_than_eps = np.abs(norm_cumsum_minus_expectation) >= eps
+    greater_than_eps = greater_than_eps.astype(int)
+    perc = 1.0 * np.sum(greater_than_eps, 0) / n_rows
 
+    plt.plot(xsis, perc, 'r', label='P[x_m-E[x]>eps')
+
+    plt.ylim([0, 1]) #if I wish to see only the error graph more clearly
+    if eps == 0.001:
+        plt.legend(loc='lower right')
+    else:
+        plt.legend(loc='upper right')
     plt.show()
-
-
-
-
